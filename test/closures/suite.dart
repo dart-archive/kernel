22b8636c61c537691d6ab27e010493d92f982c90
Peter von der Ahé <ahe@google.com>
2016-10-03 14:27:46 +0200

    Write transformed dill file and run it.
    R=karlklose@google.com
    
    Review URL: https://chromereviews.googleplex.com/515427013 .

--- test/closures/suite.dart
+++ test/closures/suite.dart
@@ -60,11 +60,16 @@ import 'package:kernel/checks.dart' show
 import 'package:kernel/transformations/closure_conversion.dart' as
     closure_conversion;
 
+import 'package:kernel/binary/ast_to_binary.dart' show
+    BinaryPrinter;
+
 const bool generateExpectations =
     const bool.fromEnvironment("generateExpectations");
 
 class TestContext extends SuiteContext {
   final String sdk;
+
+  final Uri vm;
 
   final String packageRoot;
 
@@ -78,42 +83,76 @@ class TestContext extends SuiteContext {
       const ClosureConversion(),
       const Print(),
       const MatchExpectation<TestContext>(".expect"),
+      const Run(),
   ];
 
-  TestContext(this.sdk, this.packageRoot, this.strongMode, this.dartSdk);
+  TestContext(this.sdk, this.vm, this.packageRoot, this.strongMode,
+      this.dartSdk);
 
   AnalysisContext createAnalysisContext() {
     return createContext(sdk, packageRoot, strongMode, dartSdk: dartSdk);
   }
 }
 
-Future<TestContext> createSuiteContext(Compilation suite) async {
-  String sdk = Platform.environment["DART_AOT_SDK"];
-  if (sdk == null) {
-    throw "The environment variable 'DART_AOT_SDK' isn't defined, "
-        "it should point to a patched SDK.";
+enum Environment {
+  directory,
+  file,
+}
+
+Future<String> getEnvironmentVariable(
+    String name, Environment kind, String undefined, notFound(String n)) async {
+  String result = Platform.environment[name];
+  if (result == null) {
+    throw undefined;
   }
+  switch (kind) {
+    case Environment.directory:
+      if (!await new Directory(result).exists()) throw notFound(result);
+      break;
+
+    case Environment.file:
+      if (!await new File(result).exists()) throw notFound(result);
+      break;
+  }
+  return result;
+}
+
+Future<bool> fileExists(Uri base, String path) async {
+  return await new File.fromUri(base.resolve(path)).exists();
+}
+
+Future<TestContext> createSuiteContext(Compilation suite) async {
   const String suggestion =
       "Try checking the value of environment variable 'DART_AOT_SDK', "
       "it should point to a patched SDK.";
-  if (!await new Directory(sdk).exists()) {
-    throw "Couldn't locate '$sdk'. $suggestion";
-  }
-  Future<bool> fileExists(Uri base, String path) async {
-    return await new File.fromUri(base.resolve(path)).exists();
-  }
+  String sdk = await getEnvironmentVariable(
+      "DART_AOT_SDK", Environment.directory,
+      "Please define environment variable 'DART_AOT_SDK' to point to a "
+      "patched SDK.",
+      (String n) => "Couldn't locate '$n'. $suggestion");
   Uri sdkUri = Uri.base.resolve("$sdk/");
-  if (!await fileExists(sdkUri, "lib/async/async.dart")) {
-    throw "Couldn't find 'lib/async/async.dart' in '$sdk'. $suggestion";
+  const String asyncDart = "lib/async/async.dart";
+  if (!await fileExists(sdkUri, asyncDart)) {
+    throw "Couldn't find '$asyncDart' in '$sdk'. $suggestion";
   }
-  if (await fileExists(sdkUri, "lib/async/async_sources.gypi")) {
-    throw "Found 'lib/async/async_sources.gypi' in '$sdk', "
-        "so it isn't a patched SDK. $suggestion";
+  const String asyncSources = "lib/async/async_sources.gypi";
+  if (await fileExists(sdkUri, asyncSources)) {
+    throw "Found '$asyncSources' in '$sdk', so it isn't a patched SDK. "
+        "$suggestion";
   }
+
+  String vmPath = await getEnvironmentVariable("DART_AOT_VM", Environment.file,
+      "Please define environment variable 'DART_AOT_VM' to point to a "
+      "Dart VM that reads .dill files.",
+      (String n) => "Couldn't locate '$n'. Please check the value of "
+          "environment variable 'DART_AOT_VM', it should point to a "
+          "Dart VM that reads .dill files.");
+  Uri vm = Uri.base.resolve(vmPath);
+
   String packageRoot = Uri.base.resolve("packages").toFilePath();
   bool strongMode = false;
   return new TestContext(
-      sdk, packageRoot, strongMode, createDartSdk(sdk, strongMode));
+      sdk, vm, packageRoot, strongMode, createDartSdk(sdk, strongMode));
 }
 
 class Kernel extends Step<TestDescription, Program, TestContext> {
@@ -132,6 +171,10 @@ class Kernel extends Step<TestDescription, Program, TestContext> {
           getTarget("vm", new TargetFlags(strongMode: testContext.strongMode));
       Program program =
           loader.loadProgram(description.file.path, target: target);
+      for (var error in loader.errors) {
+        return new Result<Program>.fail(program, "$error");
+      }
+      target.transformProgram(program);
       // TODO(ahe): Use `runSanityChecks` when merging with master.
       CheckParentPointers.check(program);
       return new Result<Program>.pass(program);
@@ -171,14 +214,15 @@ class ClosureConversion extends Step<Program, Program, TestContext> {
   }
 }
 
-class MatchExpectation<C extends SuiteContext> extends Step<Program, Null, C> {
+class MatchExpectation<C extends SuiteContext>
+    extends Step<Program, Program, C> {
   final String suffix;
 
   const MatchExpectation(this.suffix);
 
   String get name => "match expectations";
 
-  Future<Result<Null>> run(Program program, C context) async {
+  Future<Result<Program>> run(Program program, C context) async {
     Library library = program.mainMethod.parent;
     Uri uri = library.importUri;
     StringBuffer buffer = new StringBuffer();
@@ -189,21 +233,62 @@ class MatchExpectation<C extends SuiteContext> extends Step<Program, Null, C> {
       String expected = await expectedFile.readAsString();
       if (expected.trim() != "$buffer".trim()) {
         String diff = await runDiff(expectedFile.uri, "$buffer");
-        return new Result<Null>.fail(
+        return new Result<Program>.fail(
             null, "$uri doesn't match ${expectedFile.uri}\n$diff");
       }
     } else if (generateExpectations) {
       await openWrite(expectedFile.uri, (IOSink sink) {
           sink.writeln("$buffer".trim());
         });
-      return new Result<Null>.fail(null, "Generated ${expectedFile.uri}");
+      return new Result<Program>.fail(program, "Generated ${expectedFile.uri}");
     } else {
-      return new Result<Null>.fail(null, """
+      return new Result<Program>.fail(program, """
 Please create file ${expectedFile.path} with this content:
 $buffer""");
     }
-    return new Result<Null>.pass(null);
+    return new Result<Program>.pass(program);
   }
+}
+
+class Run extends Step<Program, int, TestContext> {
+  const Run();
+
+  String get name => "run";
+
+  Future<Result<int>> run(Program program, TestContext context) async {
+    Directory tmp = await Directory.systemTemp.createTemp();
+    int exitCode;
+    StringBuffer sb;
+    try {
+      File generated = new File.fromUri(tmp.uri.resolve("generated.dill"));
+      IOSink sink = generated.openWrite();
+      try {
+        new BinaryPrinter(sink).writeProgramFile(program);
+      } finally {
+        print("Wrote `${generated.path}`");
+        await sink.close();
+      }
+      Process process = await Process.start(
+          context.vm.toFilePath(), [generated.path, "Hello, World!"]);
+      process.stdin.close();
+      Future<List<String>> stdoutFuture =
+          process.stdout.transform(UTF8.decoder).toList();
+      Future<List<String>> stderrFuture =
+          process.stderr.transform(UTF8.decoder).toList();
+      exitCode = await process.exitCode;
+      sb = new StringBuffer();
+      sb.writeAll(await stdoutFuture);
+      sb.writeAll(await stderrFuture);
+    } finally {
+      tmp.delete(recursive: true);
+    }
+    if (exitCode == 0) {
+      return new Result<int>.pass(exitCode);
+    } else {
+      return new Result<int>.fail(exitCode, "$sb");
+    }
+  }
+
 }
 
 Future<String> runDiff(Uri expected, String actual) async {
