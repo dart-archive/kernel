5c0bc1c45f04d26df2b55ca77e32de4a867a2919
Peter von der Ahé <ahe@google.com>
2016-10-04 10:20:26 +0200

    Add test case for type variables in hoisted function.
    R=karlklose@google.com
    
    Review URL: https://chromereviews.googleplex.com/515497013 .

---
+++ test/closures/type_variables.dart
@@ -0,0 +1,15 @@
+// Copyright (c) 2016, the Dart project authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE.md file.
+
+class C<T> {
+  foo() => (T x) {
+    T y = x;
+    Object z = y;
+    return z as T;
+  };
+}
+
+main(arguments) {
+  print(new C<String>().foo()(arguments.first));
+}
